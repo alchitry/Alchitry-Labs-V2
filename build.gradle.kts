0b0c9788dbfde7fed6e9813c2f1ab17a61df8b10
Justin Rajewski <justin@alchitry.com>
2023-08-25 14:34:26 -0600

    Added cross platform dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.7.1")
-    implementation(compose.desktop.currentOs)
+    implementation(compose.desktop.linux_x64)
+    implementation(compose.desktop.linux_arm64)
+    implementation(compose.desktop.windows_x64)
+    implementation(compose.desktop.macos_x64)
+    implementation(compose.desktop.macos_arm64)
     implementation(kotlin("reflect"))
     implementation("org.jdom:jdom2:2.0.6.1")
 
@@ -83,22 +87,20 @@ tasks.named("processResources") {
     dependsOn("generateVersionProperties")
 }
 
-tasks {
-    val fatJar = register<Jar>("fatJar") {
-        dependsOn.addAll(
-            listOf(
-                "compileJava",
-                "compileKotlin",
-                "processResources"
-            )
-        ) // We need this for Gradle optimization to work
-        archiveClassifier.set("standalone") // Naming the jar
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        manifest { attributes(mapOf("Main-Class" to "com.alchitry.labs.MainKt")) } // Provided we set it up in the application plugin configuration
-        val sourcesMain = sourceSets.main.get()
-        val contents = configurations.runtimeClasspath.get()
-            .map { if (it.isDirectory) it else zipTree(it) } +
-                sourcesMain.output
-        from(contents)
-    }
+tasks.register<Jar>("fatJar") {
+    dependsOn.addAll(
+        listOf(
+            "compileJava",
+            "compileKotlin",
+            "processResources"
+        )
+    ) // We need this for Gradle optimization to work
+    archiveClassifier.set("standalone") // Naming the jar
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest { attributes(mapOf("Main-Class" to "com.alchitry.labs.MainKt")) }
+    val sourcesMain = sourceSets.main.get()
+    val contents = configurations.runtimeClasspath.get()
+        .map { if (it.isDirectory) it else zipTree(it) } +
+            sourcesMain.output
+    from(contents)
 }
