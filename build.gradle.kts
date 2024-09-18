d08c777f7beb51adc7413973c0d74df1fb059417
Justin Rajewski <justin@alchitry.com>
2024-09-18 15:33:20 -0600

    2.0.15 Beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
     id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
-val fullVersion = "2.0.14-BETA"
+val fullVersion = "2.0.15-BETA"
 val numOnlyVersion = fullVersion.split('-').first()
 
 group = "com.alchitry"
