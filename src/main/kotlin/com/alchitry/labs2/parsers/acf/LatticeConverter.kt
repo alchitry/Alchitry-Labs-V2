2c0b68700dd4493295669e0fb64599c0fb22309c
Justin Rajewski <justin@alchitry.com>
2025-07-10 11:16:26 -0600

    Switched native constraints to go into the SDC file instead of the PCF for the Cu.

--- src/main/kotlin/com/alchitry/labs2/parsers/acf/LatticeConverter.kt
+++ src/main/kotlin/com/alchitry/labs2/parsers/acf/LatticeConverter.kt
@@ -13,44 +13,32 @@ data object LatticeConverter : AcfConverter {
         board: Board,
         constraints: List<Constraint>
     ): List<NativeConstraint> {
-        val pinConstraints = constraints.filterIsInstance<Constraint.PinConstraint>()
+        var index = 0
         val sdc = buildString {
-            pinConstraints.filter { it.attributes.any { attr -> attr is PinAttribute.Frequency } }
-                .forEachIndexed { index, clock ->
-                    val frequency = clock.attributes.first { it is PinAttribute.Frequency } as PinAttribute.Frequency
-                val portName = clock.port.flatFullPortName
-                append("# ")
-                append(portName)
-                append(" => ")
-                    append(frequency.value)
-                append("Hz\n")
-
-                append("create_clock -name ")
-                append(portName.replace("[", "_").replace("]", "_"))
-                append("_")
-                append(index)
-                append(" -period ")
-                    val nsPeriod = 1000000000.0 / frequency.value
-                append(nsPeriod)
-                append(" [get_ports ")
-                append(portName)
-                appendLine("]")
-            }
-        }
-
-        val pcf = buildString {
             constraints.forEach { constraint ->
                 when (constraint) {
                     is Constraint.PinConstraint -> {
-                        append("set_io ")
-                        append(constraint.port.flatFullPortName)
-                        append(" ")
-                        append(constraint.pin.fpgaPin)
-                        val pull = constraint.attributes.firstOfTypeOrNull<PinAttribute.Pull>()
-                        if (pull != null && pull.value == PinPull.Up) {
-                            append(" -pullup yes")
+                        if (constraint.attributes.any { attr -> attr is PinAttribute.Frequency }) {
+                            val frequency =
+                                constraint.attributes.first { it is PinAttribute.Frequency } as PinAttribute.Frequency
+                            val portName = constraint.port.flatFullPortName
+                            append("# ")
+                            append(portName)
+                            append(" => ")
+                            append(frequency.value)
+                            append("Hz\n")
+
+                            append("create_clock -name ")
+                            append(portName.replace("[", "_").replace("]", "_"))
+                            append("_")
+                            append(index++)
+                            append(" -period ")
+                            val nsPeriod = 1000000000.0 / frequency.value
+                            append(nsPeriod)
+                            append(" [get_ports ")
+                            append(portName)
+                            appendLine("]")
                         }
-                        appendLine()
                     }
 
                     is Constraint.NativeBlockConstraint -> {
@@ -58,7 +46,20 @@ data object LatticeConverter : AcfConverter {
                         appendLine()
                     }
                 }
+            }
+        }
 
+        val pcf = buildString {
+            constraints.filterIsInstance<Constraint.PinConstraint>().forEach { constraint ->
+                append("set_io ")
+                append(constraint.port.flatFullPortName)
+                append(" ")
+                append(constraint.pin.fpgaPin)
+                val pull = constraint.attributes.firstOfTypeOrNull<PinAttribute.Pull>()
+                if (pull != null && pull.value == PinPull.Up) {
+                    append(" -pullup yes")
+                }
+                appendLine()
             }
         }
 
