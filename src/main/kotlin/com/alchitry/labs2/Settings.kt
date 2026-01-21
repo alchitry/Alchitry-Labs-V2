263df7441562391f7000cdd24bd0015f0ba70eac
Justin Rajewski <Justin@alchitry.com>
2026-01-21 12:49:37 -0700

    Use icecube as default

--- src/main/kotlin/com/alchitry/labs2/Settings.kt
+++ src/main/kotlin/com/alchitry/labs2/Settings.kt
@@ -136,7 +136,7 @@ object Settings {
     var vivadoLocation by StringSetting("VIVADO_LOC", null)
     var iceCubeLocation by StringSetting("ICECUBE_LOC", null)
     var iceCubeLicense by StringSetting("ICECUBE_LICENSE", null)
-    var useIceCube by BooleanSetting("USE_ICECUBE", false)
+    var useIceCube by BooleanSetting("USE_ICECUBE", true)
 
     var uiScale by FloatSetting("UI_SCALE", 1.0f)
     var errorReporting by BooleanSetting("ERROR_REPORTING", false)
